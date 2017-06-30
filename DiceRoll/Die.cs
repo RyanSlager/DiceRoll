f99c811477b5f9c467e108731c58028ef01a0a61
danteCLRK <fryanslaucerers@gmail.com>
2017-06-30 09:56:49 -0400

    finished

---
+++ DiceRoll/Die.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoll
+{
+    class Die
+    {
+        private int min;
+        private int max;
+        private Random ran;
+
+        public Die(int min, int max, Random ran)
+        {
+            this.min = min;
+            this.max = max;
+            this.ran = ran;
+        }
+
+        public int DiceRoll()
+        { 
+
+            int x = ran.Next(min, max);
+
+            Console.WriteLine($"You rolled a {x}!");
+
+            return x;
+
+        }
+
+        public void TestSnakes(int x, int y)
+        {
+            if(x == y && x == 1 && y == 1)
+            {
+                Console.WriteLine("Snake Eyes!!!");
+            }   
+        }
+        
+        public void TestCraps(int x, int y)
+        {
+            int roll = x + y;
+
+            if(roll == 2 || roll == 3 || roll == 12)
+            {
+                Console.WriteLine("CRAPS!!!!");
+            }
+        }
+
+        public void TestCars(int x, int y)
+        {
+            if(x == 6 && y == 6)
+            {
+                Console.WriteLine("BoxCars!");
+            }
+        }
+
+    }
+}
--- DiceRoll/Program.cs
+++ DiceRoll/Program.cs
@@ -10,6 +10,58 @@ namespace DiceRoll
     {
         static void Main(string[] args)
         {
+            bool cont = true;
+
+            while (cont == true)
+            {
+                string rawSides;
+                int parSides;
+                Random ran = new Random();
+
+                Console.WriteLine("How many sides do your die have?\n");
+                rawSides = Console.ReadLine();
+
+                while (!Int32.TryParse(rawSides, out parSides))
+                {
+                    Console.WriteLine("Please enter a number greater than 0\n");
+                    rawSides = Console.ReadLine();
+                }
+
+                Die dice1 = new Die(1, parSides, ran);
+                Die dice2 = new Die(1, parSides, ran);
+
+                int roll1 = dice1.DiceRoll();
+                int roll2 = dice2.DiceRoll();
+
+                dice1.TestSnakes(roll1, roll2);
+                dice1.TestCars(roll1, roll2);
+                dice1.TestCraps(roll1, roll2);
+
+                cont = Continue();
+            }
+        }
+
+        public static Boolean Continue()
+        {
+            Boolean run;
+            Console.WriteLine("Continue? Y/N");
+            String answer = Console.ReadLine();
+
+            if ((answer == "Y") || (answer == "y"))
+            {
+                run = true;
+            }
+            else if ((answer == "N") || (answer == "n"))
+            {
+                run = false;
+                Console.WriteLine("Good Bye!");
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry I don't understand. Let's try again");
+                run = Continue();
+            }
+            return run;
         }
     }
 }
